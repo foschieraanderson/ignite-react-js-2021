656d354a319a4b2ebbc2f98e3eccfd182413825c
foschieraanderson <foschieraanderson@gmail.com>
2021-03-26 03:10:59 -0300

    Listando transações e seeds da aplicação dtMoney

--- II-chapter/dtmoney/src/index.tsx
+++ II-chapter/dtmoney/src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 7000,
+          createdAt: new Date('2021-02-17 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 15:30:52'),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
