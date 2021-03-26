3e33510151d759a0e6cfefc6eacf42ae4504cec4
foschieraanderson <foschieraanderson@gmail.com>
2021-03-26 16:41:48 -0300

    Usando Context API na aplicação dtMoney

--- II-chapter/dtmoney/src/App.tsx
+++ II-chapter/dtmoney/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionsContext } from "./TransactionsContext";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
@@ -19,7 +20,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
@@ -29,6 +30,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
