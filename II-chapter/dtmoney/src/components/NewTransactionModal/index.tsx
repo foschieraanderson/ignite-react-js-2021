1a6d916a9818c19c4ef4a6228629de2baaf3abb3
foschieraanderson <foschieraanderson@gmail.com>
2021-03-26 02:54:15 -0300

    Inserindo transação na API da aplicação dtMoney

--- II-chapter/dtmoney/src/components/NewTransactionModal/index.tsx
+++ II-chapter/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from "react";
 import Modal from "react-modal";
+import { api } from "../../services/api";
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
@@ -19,12 +20,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
-      type
-    })
+      type,
+    };
+
+    api.post('transactions', data)
   }
 
   return (
