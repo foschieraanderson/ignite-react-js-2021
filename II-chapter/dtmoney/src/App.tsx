69592975be88f8212f8e4c213da12da13fdccc5b
foschieraanderson <foschieraanderson@gmail.com>
2021-03-26 17:02:26 -0300

    Carregando transações no context da aplicação dtMoney

--- II-chapter/dtmoney/src/App.tsx
+++ II-chapter/dtmoney/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
@@ -20,7 +20,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
@@ -30,6 +30,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
